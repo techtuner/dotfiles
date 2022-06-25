4213f46ec0bd93f647e5c9e3b9654224c24529ae
techtuner <techtuner284@gmail.com>
2022-06-25 20:39:23 +0530

    Add Visual Studio Code Setting

---
+++ code/extension.sh
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------------------------
+#
+# Script to install all extension in VSCode
+#
+# -----------------------------------------------------------------------------
+
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-python.vscode-pylance
+code --install-extension ritwickdey.liveserver
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension formulahendry.code-runner
+code --install-extension formulahendry.auto-rename-tag
+code --install-extension formulahendry.auto-complete-tag
+code --install-extension formulahendry.auto-close-tag
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension alefragnani.project-manager
+code --install-extension christian-kohler.path-intellisense
+code --install-extension golang.go
+code --install-extension rust-lang.rust
+code --install-extension pkief.material-product-icons
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension aaron-bond.better-comments
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension mikestead.dotenv
+code --install-extension xabikos.JavaScriptSnippets
+code --install-extension humao.rest-client
+code --install-extension gruntfuggly.todo-tree
+code --install-extension wayou.vscode-todo-highlight
+code --install-extension piousdeer.horizon-theme-vscode-default-token-colors
---
+++ code/insider-extension.sh
@@ -0,0 +1,32 @@
+# -----------------------------------------------------------------------------
+#
+# Script to install all extension in VSCode
+#
+# -----------------------------------------------------------------------------
+
+code-insiders --install-extension ms-python.python
+code-insiders --install-extension ms-vscode.cpptools
+code-insiders --install-extension ms-python.vscode-pylance
+code-insiders --install-extension ritwickdey.liveserver
+code-insiders --install-extension dbaeumer.vscode-eslint
+code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension ms-azuretools.vscode-docker
+code-insiders --install-extension formulahendry.code-runner
+code-insiders --install-extension formulahendry.auto-rename-tag
+code-insiders --install-extension formulahendry.auto-complete-tag
+code-insiders --install-extension formulahendry.auto-close-tag
+code-insiders --install-extension catppuccin.catppuccin-vsc
+code-insiders --install-extension alefragnani.project-manager
+code-insiders --install-extension christian-kohler.path-intellisense
+code-insiders --install-extension golang.go
+code-insiders --install-extension rust-lang.rust
+code-insiders --install-extension pkief.material-product-icons
+code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension aaron-bond.better-comments
+code-insiders --install-extension yzhang.markdown-all-in-one
+code-insiders --install-extension mikestead.dotenv
+code-insiders --install-extension xabikos.JavaScriptSnippets
+code-insiders --install-extension humao.rest-client
+code-insiders --install-extension gruntfuggly.todo-tree
+code-insiders --install-extension wayou.vscode-todo-highlight
+code-insiders --install-extension piousdeer.horizon-theme-vscode-default-token-colors
