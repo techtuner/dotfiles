0ac4bab336c66574beda56209d3f700eb3371837
techtuner <techtuner284@gmail.com>
2023-07-22 16:02:56 +0530

    Add Code Extension script

---
+++ code_extension.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+code --install-extension sumneko.lua
+code --install-extension ritwickdey.liveserver
+code --install-extension golang.go
+code --install-extension miguelsolorio.fluent-icons
+code --install-extension miguelsolorio.symbols
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension christian-kohler.path-intellisense
+code --install-extension ms-python.python
+code --install-extension ms-python.black-formatter
+code --install-extension aaron-bond.better-comments
+code --install-extension rust-lang.rust-analyzer
+code --install-extension ms-python.vscode-pylance
+code --install-extension vadimcn.vscode-lldb
+code --install-extension bungcip.better-toml
+code --install-extension serayuzgur.crates
+code --install-extension inci-august.august-themes
+code --install-extension kisstkondoros.vscode-gutter-preview
+code --install-extension usernamehw.errorlens
