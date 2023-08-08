9c5f9b9011f821038e3b12c1504f4ec96baba4f8
techtuner <techtuner284@gmail.com>
2023-08-08 19:43:37 +0530

    Update Ubuntu Installation Script

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,12 +2,6 @@
 
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
-# Add Code to Source Lists
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo sh -c echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
-rm -rf packages.microsoft.gpg
-
 # Github CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -15,7 +9,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 sudo apt autoremove -y
-sudo apt-get install curl code wget gh terminator dconf-editor gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc gh python3-pip -y
+sudo apt-get install curl wget gh terminator dconf-editor gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc gh python3-pip -y
+sudo snap install code --classic
+sudo snap install nvim --classic
 sudo apt remove apport apport-gtk -y && sudo apt purge apport apport-gtk -y
 
 code&
@@ -32,6 +28,7 @@ cp -r ./nvim/ ~/.config/
 cp -r ./terminator/ ~/.config/
 cp -r ./code/settings.json ~/.config/Code/User/
 cp -r ./code/keybindings.json ~/.config/Code/User/
+
 
 sudo chmod +x ./code_extension.sh
 ./code_extension.sh
