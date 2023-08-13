a5f7de1aa7bd1b802883fdf5b4e0a12a90a50f77
techtuner <techtuner284@gmail.com>
2023-08-13 12:45:50 +0530

    Add Zsh Configurations

---
+++ zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias src="source ~/.zshrc"
+alias python="python3"
+alias pip="pip3"
+alias e="exit"
+alias dots="cd ~/Downloads/kali-dots && ls"
+alias htbo="sudo openvpn ~/vpns/htb.ovpn"
+alias thmo="sudo openvpn ~/vpns/thm.ovpn"
