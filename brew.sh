c1ab748ffb4332531aa725ab1e7137eba2bfb0dc
techtuner <techtuner284@gmail.com>
2023-08-19 10:40:42 +0530

    Update Brew Installation script

--- brew.sh
+++ brew.sh
@@ -4,8 +4,10 @@
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 sudo apt autoremove -y
 
-sudo apt-get install wget build-essential procps curl file -y
+sudo apt-get install wget build-essential procps curl file zsh -y
 
+# installing Oh-My-Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Installing LinuxBrew (HomeBrew)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -14,4 +16,18 @@ test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/bre
 test -r ~/.bash_profile && echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bash_profile
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.profile
 
+source ~/.profile
 
+# Installing applications
+brew update
+brew install gcc
+brew install --cask brave-browser
+brew install --cask alacritty
+
+
+
+# Create configuration Directories
+mkdir ~/.config/alacritty/
+
+# Copy Configuration files
+sudo cp -r ./alacritty/* ~/.config/alacritty/
