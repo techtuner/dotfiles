295ca1606720c58359e41992b14461d3c9a2d471
Santhosh Pai <techtuner284@gmail.com>
2023-09-14 09:11:06 +0530

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias e="exit"
 alias dots="cd ~/Downloads/kali-dots && ls"
 alias htbo="sudo openvpn ~/vpns/htb.ovpn"
 alias thmo="sudo openvpn ~/vpns/thm.ovpn"
+alias tmux="tmux -u"  # Fixes tmux not showing symbols in the terminal
