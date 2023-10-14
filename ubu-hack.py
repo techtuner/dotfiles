b89dac276ed3f3e2f65c8855f10fe1f8edd1b670
techtuner <techtuner284@gmail.com>
2023-10-14 11:15:02 +0530

    Update Ubuntu script and Add Bash Aliases

--- ubu-hack.py
+++ ubu-hack.py
@@ -10,7 +10,7 @@ git_tools_path = f"{tools_path}/GitTools"
 
 def update_system():
     os.system("sudo apt-get update -y")
-    os.system("sudo apt-get upgrade -y")
+    os.system("sudo apt-get full-upgrade -y")
     os.system("sudo apt autoremove -y")
 
 
@@ -23,7 +23,7 @@ def install_apps():
     os.system("sudo snap install code --classic")
     os.system("sudo snap install nvim --classic")
     os.system("sudo snap install spotify --classic")
-    apps = "bless adb python3-pip3 curl dnsrecon gobuster python3-impacket nbtscan nikto onesixtyone redis-tools smbclient smbmap snmp sslscan sipvicious whatweb wkhtmltopdf libimage-exiftool-perl golang-go python3-ldap3 python3-yaml ldnsutils strace dnsutils yersinia dhcpstarv steghide protobuf-compiler httrack ruby"
+    apps = "bless adb python3-pip3 curl dnsrecon gobuster python3-impacket nbtscan nikto onesixtyone redis-tools smbclient smbmap snmp sslscan sipvicious whatweb wkhtmltopdf libimage-exiftool-perl golang-go python3-ldap3 python3-yaml ldnsutils strace dnsutils yersinia dhcpstarv steghide protobuf-compiler httrack ruby libglib2.0-dev openvpn"
     app_list = apps.split(" ")
     for app in app_list:
         os.system(f"sudo apt-get install {app} -y")
@@ -33,19 +33,13 @@ def install_apps():
     os.system("chmod +x ./source_files.sh")
     subprocess.call(["sh", f"{current_path}/source_files.sh"])
     os.system(
-        "sudo python3 -m pip3 install git+https://github.com/Tib3rius/AutoRecon.git"
+        "sudo python3 -m pip install git+https://github.com/Tib3rius/AutoRecon.git"
     )
     os.chdir(tools_path)
     os.system("sudo wget https://download.sysinternals.com/files/PSTools.zip")
     os.system("sudo mkdir PSTools")
     os.system("sudo unzip PSTools.zip -d PSTools")
     os.system("sudo rm -rf PSTools.zip")
-    os.chdir(tools_path)
-    os.system(
-        "sudo wget https://github.com/neovim/neovim/releases/download/v0.9.0/nvim.appimage"
-    )
-    os.system(f"sudo chmod +x {tools_path}/nvim.appimage")
-    os.system(f"sudo ln -s {tools_path}/nvim.appimage /usr/bin/nvim")
     os.chdir(tools_path)
     os.system(
         f"sudo wget https://storage.googleapis.com/caido-releases/v0.28.0/caido-desktop-linux-v0.28.0-9799aa77.AppImage"
@@ -81,7 +75,7 @@ def install_apps():
     )
     os.chdir(tools_path)
     os.system(
-        "python3 -m pip3 install --upgrade git+https://github.com/fportantier/habu.git"
+        "python3 -m pip install --upgrade git+https://github.com/fportantier/habu.git"
     )
     os.system("go install github.com/gophish/gophish@latest")
     os.system("sudo git clone https://github.com/cwinfosec/revshellgen.git")
@@ -99,12 +93,11 @@ def install_apps():
     os.system("sudo git clone https://github.com/bahatiphill/BillCipher")
     os.chdir(f"{tools_path}/BillCipher")
     os.system("pip3 install -r requirements.txt")
-    os.system("pip3 install -r requirements.txt")
     os.system(f"sudo chmod +x {tools_path}/BillCipher/")
     os.chdir(tools_path)
     os.system("sudo git clone https://github.com/sherlock-project/sherlock.git")
     os.chdir(f"{tools_path}/sherlock")
-    os.system(f"python3 -m pip3 install -r requirements.txt")
+    os.system(f"python3 -m pip install -r requirements.txt")
     os.system(f"sudo chmod +x {tools_path}/sherlock/sherlock/sherlock.py")
     os.system(
         f"sudo ln -s {tools_path}/sherlock/sherlock/sherlock.py /usr/bin/sherlock"
@@ -112,7 +105,7 @@ def install_apps():
     os.chdir(tools_path)
     os.system("sudo git clone https://github.com/s0md3v/Photon")
     os.chdir(f"{tools_path}/Photon")
-    os.system(f"python3 -m pip3 install -r requirements.txt")
+    os.system(f"python3 -m pip install -r requirements.txt")
     os.system(f"sudo chmod +x {tools_path}/Photon/photon.py")
     os.chdir(tools_path)
     os.system("sudo git clone https://github.com/Moham3dRiahi/Th3inspector.git")
@@ -167,7 +160,7 @@ def install_apps():
     os.system(f"sudo ln -s {home}/go/bin/grpcurl /usr/bin/grpcurl")
     os.chdir(tools_path)
     os.system("sudo git clone https://github.com/TebbaaX/GRecon.git")
-    os.chdir(f"cd {tools_path}/GRecon")
+    os.chdir(f"{tools_path}/GRecon")
     os.system("sudo pip3 install -r requirements.txt")
     os.chdir(tools_path)
 
@@ -187,12 +180,11 @@ def install_apps():
     os.system("sudo chmod +x superenum")
     os.system(f"sudo ln -s {tools_path}/SuperEnum/superenum /usr/bin/superenum")
     os.chdir(tools_path)
-    os.system(
-        f"git clone https://github.com/zsh-users/zsh-syntax-highlighting.git {home}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-    )
-    os.system(
-        f"git clone https://github.com/zsh-users/zsh-autosuggestions {home}/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-    )
+    os.system("sudo git clone https://github.com/threat9/routersploit")
+    os.chdir(f"{tools_path}/routersploit")
+    os.system("python3 -m pip install -r requirements.txt")
+    os.system("python3 -m pip install bluepy")
+    os.system(f"sudo ln -s {tools_path}/routersploit/rsf.py /usr/bin/routersploit")
     os.chdir(current_path)
 
 
@@ -211,6 +203,7 @@ def labs():
 def copy_configs():
     os.system(f"{current_path}/wallpapers {home}/Pictures/")
     os.system(f"sudo cp -r {current_path}/nvim/ {home}/.config")
+    os.system(f"sudo cp -r {current_path}/.bashrc {home}")
 
 
 def code_extensions():
