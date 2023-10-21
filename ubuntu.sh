85e5678031e0892229dbb50196d27841572acedb
techtuner <techtuner284@gmail.com>
2023-10-21 18:10:29 +0530

    Update Ubuntu Script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cwd=$pwd
+
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
 # Github CLI
@@ -8,13 +10,20 @@ sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 
 # Install Rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo/env"
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# source "$HOME/.cargo/env"
 
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 sudo apt autoremove -y
 
-sudo apt-get install curl wget gh dconf-editor dconf-cli net-tools gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 python3-pip -y
+sudo apt-get install curl wget cargo gh dconf-editor dconf-cli net-tools gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 python3-pip -y
+
+cd /opt
+sudo wget https://downloads.arduino.cc/arduino-ide/arduino-ide_2.2.1_Linux_64bit.AppImage
+sudo chmod +x arduino-ide_2.2.1_Linux_64bit.AppImage
+sudo ln -s /opt/https://downloads.arduino.cc/arduino-ide/arduino-ide_2.2.1_Linux_64bit.AppImage /usr/bin/arduinoide
+
+cd $cwd
 
 sudo snap install code --classic
 sudo snap install nvim --classic
@@ -28,7 +37,8 @@ code&
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc
 
-sudo cp -r ./fonts/'Fira Code' /usr/share/fonts/truetypes
+sudo cp -r ./fonts/'Fira Code' /usr/share/fonts/truetype/
+sudo cp -r ./fonts/'Dank Mono' /usr/share/fonts/opentype/
 
 sudo chmod +x ./source_files.sh
 ./source_files.sh
