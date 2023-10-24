cfb71064aa9eaca9d96e546e3a93916ec7736489
techtuner <techtuner284@gmail.com>
2023-10-24 23:27:55 +0530

    Update Ubuntu Script

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,6 +4,15 @@ cwd=$PWD
 
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 
+# Node from ppa
+sudo apt-get install ca-certificates curl gnupg -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
 # Github CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
@@ -16,26 +25,24 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githu
 sudo apt-get update -y && sudo apt-get full-upgrade -y
 sudo apt autoremove -y
 
-sudo apt-get install curl wget cargo gh dconf-editor dconf-cli net-tools gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 python3-pip -y
+sudo apt-get install curl wget cargo gh dconf-editor dconf-cli net-tools gnome-tweaks gnome-shell-extensions software-properties-common build-essential gdb gcc cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 nodejs python3-pip -y
 
 cd /opt
 sudo wget https://downloads.arduino.cc/arduino-ide/arduino-ide_2.2.1_Linux_64bit.AppImage
 sudo chmod +x arduino-ide_2.2.1_Linux_64bit.AppImage
 sudo ln -s /opt/https://downloads.arduino.cc/arduino-ide/arduino-ide_2.2.1_Linux_64bit.AppImage /usr/bin/arduinoide
 
-cd $c wd
+cd $cwd
 
 sudo snap install code --classic
 sudo snap install nvim --classic
 sudo snap install spotify --classic
 
+python3 -m pip install --upgrade pip
 sudo apt remove apport apport-gtk -y && sudo apt purge apport apport-gtk -y
 
 code&
 
-# Node Version Manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-source ~/.bashrc
 
 sudo cp -r ./fonts/'Fira Code' /usr/share/fonts/truetype/
 sudo cp -r ./fonts/'Dank Mono' /usr/share/fonts/opentype/
