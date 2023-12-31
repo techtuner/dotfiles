588c8e107af5724f16c46e0aa922a06de037bdd9
techtuner <techtuner284@gmail.com>
2023-12-31 11:22:50 +0530

    Updated Ubuntu Installation script, add aliases and .zshrc

--- ubuntu.sh
+++ ubuntu.sh
@@ -58,6 +58,7 @@ cp -r ./wallpapers/ ~/Pictures/
 
 if test $which_shell =  "zsh";then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   cp ./.zshrc ~
   cp ./powerlevel10k/.p10k.zsh ~
   echo "alias src=\"source ~/.zshrc\"" >> ~/aliases.sh
