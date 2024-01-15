6ae53c176da2a4aa16f5224138a3fb3c971ee140
techtuner <techtuner284@gmail.com>
2024-01-15 19:06:05 +0530

    Updated Powershell(pwsh) theme to match scoop and some other minor changes

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,28 +1,20 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# PSReadLines
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'ctrl+d' -Function DeleteWord
 Set-PSReadLineOption -PredictionSource History
-
-# Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
 
 # Aliases
 Set-Alias -Name vim -Value nvim
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Set-Alias e exit
 
-# Utilities
 function which ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
@@ -30,16 +22,15 @@ function which ($command) {
 function touch { Set-Content -Path ($args[0]) -Value ($null) }
 
 function gcd ($username, $repo) {
-    if (Test-Path -Path $repo)
-    {
+    if (Test-Path -Path $repo) {
         cd $repo
-    } else {
+    }
+    else {
         git clone https://www.github.com/$username/$repo && cd $repo
     }
 }
 
-oh-my-posh init pwsh --config 'C:\Program Files (x86)\oh-my-posh\themes\robbyrussell.omp.json' | Invoke-Expression
-# Invoke-Expression (&starship init powershell)
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
 
-# Removes the blue highlight for directories/folders in pwsh
+# Removes the highlighting of the folder
 $PSStyle.FileInfo.Directory = "`e[38;2;255;255;255m"
