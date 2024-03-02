51c1c124f3ef285e22da69adbbea97d41d09184e
techtuner <techtuner284@gmail.com>
2024-03-02 19:16:59 +0530

    Updated the installation script

--- i3-setup.sh
+++ i3-setup.sh
@@ -2,9 +2,12 @@
 
 echo "Installing i3 setup files"
 
-sudo apt install tmux i3 i3-wm kitty feh dmenu rofi polybar picom -y
+sudo apt install tmux i3 i3-wm kitty feh dmenu rofi polybar picom zsh -y
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cp -r ./kitty/ ~/.config/
 cp -r ./i3/ ~/.config/
@@ -12,5 +15,3 @@ cp -r ./polybar/ ~/.config/
 cp -r ./rofi/ ~/.config/
 cp -r ./picom/ ~/.config/
 cp ./tmux/.tmux.conf ~
-
-
