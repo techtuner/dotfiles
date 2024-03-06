884f18f7c44d56e05f211e7d4371cd1a2d450b90
techtuner <techtuner284@gmail.com>
2024-03-06 12:13:28 +0530

    Updated neovim configuration

--- nvim/lua/user/plugins/colors.lua
+++ nvim/lua/user/plugins/colors.lua
@@ -33,5 +33,22 @@ return {
       })
       Colors()
     end
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        transparent_mode = true
+      })
+      Colors()
+    end
   }
 }
