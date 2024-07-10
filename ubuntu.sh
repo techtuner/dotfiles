6c4199f33064959cef18169dc1bd71dd7790a2a2
techtuner <techtuner284@gmail.com>
2024-07-10 23:39:36 +0530

    Updated Ubuntu Installation Script

--- ubuntu.sh
+++ ubuntu.sh
@@ -22,18 +22,21 @@ echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+wget https://releases.hyper.is/download/deb && sudo dpkg -i deb
+
 update_system
 
-sudo apt install git curl wget lightdm net-tools dconf-editor dconf-cli software-properties-common build-essential procps file zathura i3 i3-wm dmenu polybar pulseaudio pipewire picom ca-certificates gnupg python3 python3-pip python3-venv tmux make cmake fzf zsh ripgrep fd-find zoxide bat feh gcc gdb gcc kitty docker.io docker-compose cargo adb golang gh glow -y
+sudo apt install git curl wget lightdm net-tools dconf-editor dconf-cli software-properties-common build-essential procps file zathura i3 i3-wm dmenu polybar pulseaudio pipewire picom ca-certificates gnupg python3 python3-pip python3-venv tmux make cmake fzf zsh ripgrep fd-find zoxide bat feh gcc gdb gcc kitty docker.io docker-compose cargo adb golang gh rofi wofi -y
 
 sudo apt remove apport apport-gtk -y && sudo apt purge apport apport-gtk -y
 
-cargo install eza
+cargo install eza urlencode
 
 SNAPS=("code" "nvim" "spotify")
 for apps in ${SNAPS[@]}; do
   sudo snap install $apps --classic
 done
+
 
 cd ~/Desktop/
 
@@ -45,7 +48,7 @@ sudo python3 setup.py install
 
 cd $cwd
 sudo cp -r ./fonts/'Fira Code' /usr/share/fonts/truetype/
-sudo cp -r ./fonts/Bai_Jamjuree /usr/share/fonts/truetype/
+sudo cp -r ./fonts/Hack /usr/share/fonts/truetype/
 
 code&
 
@@ -100,6 +103,7 @@ sudo chmod +x ~/.config/polybar/launch.sh
 cp ./code/settings.json ~/.config/Code/User/
 cp ./code/keybindings.json ~/.config/Code/User/
 cp -r ./kitty/ ~/.config/
+cp -r ./Hyper/hyper.js ~/.hyper.js
 cp -r ./wallpapers/ ~/Pictures/
 cp ./tmux/.tmux.conf ~
 cp -r ./zsh/ ~
