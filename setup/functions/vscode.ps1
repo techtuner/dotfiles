f68e2c4c3c14a33222d0231bba09fa392c322a0e
techtuner <techtuner284@gmail.com>
2025-08-16 11:48:06 +0530

    Update Windows Installation Script

--- setup/functions/vscode.ps1
+++ setup/functions/vscode.ps1
@@ -1,47 +1,47 @@
 function Install-VScodeExtensions {
-    Write-Host "Installing Visual Studio Extension"
-    $extensions = @(
-        "formulahendry.auto-close-tag"
-        "formulahendry.auto-rename-tag"
-        "formulahendry.auto-complete-tag"
-        "aaron-bond.better-comments"
-        "antfu.icons-carbon"
-        "formulahendry.code-runner"
-        "vadimcn.vscode-lldb"
-        "naumovs.color-highlight"
-        "ms-azuretools.vscode-docker"
-        "editorconfig.editorconfig"
-        "usernamehw.errorlens"
-        "dsznajder.es7-react-js-snippets"
-        "dbaeumer.vscode-eslint"
-        "golang.go"
-        "kisstkondoros.vscode-gutter-preview"
-        "yzhang.markdown-all-in-one"
-        "pkief.material-icon-theme"
-        "christian-kohler.npm-intellisense"
-        "christian-kohler.path-intellisense"
-        "ms-vscode.powershell"
-        "esbenp.prettier-vscode"
-        "ms-python.python"
-        "rust-lang.rust-analyzer"
-        "redhat.vscode-yaml"
-        "twxs.cmake"
-        "reiisen.hi"
-        "sumneko.lua"
-"Catppuccin.catppuccin-vsc"
-"Catppuccin.catppuccin-vsc-icons"
+  Write-Host "Installing Visual Studio Extension"
+  $extensions = @(
+    "formulahendry.auto-close-tag"
+    "formulahendry.auto-rename-tag"
+    "formulahendry.auto-complete-tag"
+    "aaron-bond.better-comments"
+    "antfu.icons-carbon"
+    "formulahendry.code-runner"
+    "vadimcn.vscode-lldb"
+    "naumovs.color-highlight"
+    "ms-azuretools.vscode-docker"
+    "editorconfig.editorconfig"
+    "usernamehw.errorlens"
+    "dsznajder.es7-react-js-snippets"
+    "dbaeumer.vscode-eslint"
+    "golang.go"
+    "kisstkondoros.vscode-gutter-preview"
+    "yzhang.markdown-all-in-one"
+    "pkief.material-icon-theme"
+    "christian-kohler.npm-intellisense"
+    "christian-kohler.path-intellisense"
+    "ms-vscode.powershell"
+    "esbenp.prettier-vscode"
+    "ms-python.python"
+    "rust-lang.rust-analyzer"
+    "redhat.vscode-yaml"
+    "twxs.cmake"
+    "reiisen.hi"
+    "sumneko.lua"
+    "ms-vscode.cpptools"
+    "sainnhe.gruvbox-material"
+    "Catppuccin.catppuccin-vsc-icons"
 
-    )
+  )
 
-    Write-Host "Installing VS Code Extensions..."
-    foreach ($extension in $extensions) {
-        try {
-            code --install-extension $extension --force
-            Write-Host "Installed: $extension"
-        }
-        catch {
-            Write-Warning "Failed to install the $extension : $_"
-        } 
+  Write-Host "Installing VS Code Extensions..."
+  foreach ($extension in $extensions) {
+    try {
+      code --install-extension $extension --force
+      Write-Host "Installed: $extension"
     }
+    catch {
+      Write-Warning "Failed to install the $extension : $_"
+    }
+  }
 }
-
--- setup/functions/winget.ps1
+++ setup/functions/winget.ps1
@@ -1,86 +1,110 @@
 . "$PSSCriptRoot/show-progress.ps1"
 
 function Install-AppxSilent {
-     param ([string]$url)
+  param ([string]$url)
 
-    $file = "$env:TEMP\" + ($url -split "/")[-1]
-    Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing -ErrorAction SilentlyContinue
-    Add-AppxPackage -Path $file -ErrorAction SilentlyContinue > $null
+  $file = "$env:TEMP\" + ($url -split "/")[-1]
+  Invoke-WebRequest -Uri $url -OutFile $file -UseBasicParsing -ErrorAction SilentlyContinue
+  Add-AppxPackage -Path $file -ErrorAction SilentlyContinue > $null
 }
 
 
 
 function Install-Winget {
-    Write-Host "Installing Winget Application"
-    showProgress
+  Write-Host "Installing Winget Application"
+  showProgress
+
+  if (Get-Command winget -ErrorAction SilentlyContinue) {
+    Write-Host "Winget is already installed"
+  }
+  else {
+    Write-Host "Winget is not installed. Installing..."
+    $wingetURL = "https://aka.ms/getwinget"
+    $downloadPath = "$env:TEMP\winget.msixbundle"
+
+
+    $frameworks = @(
+      "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx",
+      "https://aka.ms/Microsoft.UI.Xaml.2.7.appx"
+    )
+    foreach ($url in $frameworks) {
+      Install-AppxSilent -url $url
+    }
+
+    Invoke-WebRequest -Uri $wingetURL -OutFile $downloadPath -UseBasicParsing -ErrorAction SilentlyContinue
+    Add-AppxPackage -Path $downloadPath -ErrorAction SilentlyContinue > $null
 
     if (Get-Command winget -ErrorAction SilentlyContinue) {
-        Write-Host "Winget is already installed"
+      Write-Host "Winget installed successfully!"
+      exit 1
     }
     else {
-        Write-Host "Winget is not installed. Installing..."
-        $wingetURL="https://aka.ms/getwinget"
-        $downloadPath = "$env:TEMP\winget.msixbundle"
-
-        
-        $frameworks = @(
-            "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx",
-            "https://aka.ms/Microsoft.UI.Xaml.2.7.appx"
-            )
-            foreach ($url in $frameworks) {
-                Install-AppxSilent -url $url
-            }
-            
-            Invoke-WebRequest -Uri $wingetURL -OutFile $downloadPath -UseBasicParsing -ErrorAction SilentlyContinue
-    Add-AppxPackage -Path $downloadPath -ErrorAction SilentlyContinue > $null
-    
-     if (Get-Command winget -ErrorAction SilentlyContinue) {
-        Write-Host "Winget installed successfully!"
-        exit 1
-    } else {
-        Write-Host "Failed to install Winget. Please check requirements (Windows 10 1809+)."
+      Write-Host "Failed to install Winget. Please check requirements (Windows 10 1809+)."
     }
-}
+  }
 }
 
 function Install-WingetApps {
-   $apps=@(
-    @{Name = "Brave Browser"; Id = "Brave.Brave"},
-    @{Name = "Microsoft Visual Studio Code"; Id = "Microsoft.VisualStudioCode"},
-    @{Name = "7-zip"; Id = "7zip.7zip"},
-    @{Name = "Git"; Id = "Git.Git"},
-    @{Name = "Flameshot"; Id = "Flameshot.Flameshot"},
-    @{Name = "Spotify"; Id = "Spotify.Spotify"},
-    @{Name = "VMWare Workstation Pro"; Id = "VMware.WorkstationPro"},
-    @{Name = "Google Drive"; Id = "Google.Drive"},
-    @{Name = "VLC Media Player"; Id = "VideoLAN.VLC"},
-    @{Name = "Microsoft Teams"; Id = "Microsoft.Teams"},
-    @{Name = "Zotero"; Id = "DigitalScholar.Zotero"},
-    @{Name = "Obsidian"; Id = "Obsidian.Obsidian"},
-    @{Name = "Steam"; Id = "Valve.Steam"},
-    @{Name = "Adobe Acrobat Reader"; Id = "Adobe.Acrobat.Reader.64-bit"},
-    @{Name = "Wireshark"; Id = "WiresharkFoundation.Wireshark"},
-    @{Name = "Rust"; Id = "Rustlang.Rustup"}
-   )
+  $documentFolder = [Environment]::GetFolderPath("MyDocuments")
+  $downloadFolder = [Environment]::GetFolderPath("Downloads")
 
-    foreach ($app in $apps) {
-        $name = $app.Name
-        $id = $app.Id
+  $apps = @(
+    @{Name = "Brave Browser"; Id = "Brave.Brave" },
+    @{Name = "Microsoft Visual Studio Code"; Id = "Microsoft.VisualStudioCode" },
+    @{Name = "7-zip"; Id = "7zip.7zip" },
+    @{Name = "Git"; Id = "Git.Git" },
+    @{Name = "Flameshot"; Id = "Flameshot.Flameshot" },
+    @{Name = "VLC Media Player"; Id = "VideoLAN.VLC" },
+    @{Name = "Microsoft Teams"; Id = "Microsoft.Teams" },
+    @{Name = "Zotero"; Id = "DigitalScholar.Zotero" },
+    @{Name = "Obsidian"; Id = "Obsidian.Obsidian" },
+    @{Name = "Adobe Acrobat Reader"; Id = "Adobe.Acrobat.Reader.64-bit" },
+    @{Name = "Wireshark"; Id = "WiresharkFoundation.Wireshark" },
+    @{Name = "Rust"; Id = "Rustlang.Rustup" }
+  )
 
-$isInstalled = winget list --id $id | Select-String "$id"
+  foreach ($app in $apps) {
+    $name = $app.Name
+    $id = $app.Id
 
-        if (-not $isInstalled) {
-            Write-Host "Installing $name..."
-            try{
-                winget install --id "$id" --exact --silent  --force --accept-source-agreements --accept-package-agreements
-                Write-Host "$name installed successfully"
-            }
-            catch {
-                Write-Warning "Failed to install $name : $_"
-            }
-        }
-        else {
-            Write-Host "$name is already installed"
-        }
+    $isInstalled = winget list --id $id | Select-String "$id"
+
+    if (-not $isInstalled) {
+      Write-Host "Installing $name..."
+      try {
+        winget install --id "$id" --exact --silent  --force --accept-source-agreements --accept-package-agreements
+        Write-Host "$name installed successfully"
+      }
+      catch {
+        Write-Warning "Failed to install $name : $_"
+      }
     }
+    else {
+      Write-Host "$name is already installed"
+    }
+  }
+
+  # Installing Spoify and Winlibs
+  # Setting Mingw
+
+  Invoke-WebRequest `
+    "https://github.com/brechtsanders/winlibs_mingw/releases/download/15.2.0posix-13.0.0-msvcrt-r1/winlibs-x86_64-posix-seh-gcc-15.2.0-mingw-w64msvcrt-13.0.0-r1.zip" `
+    -OutFile "$(Join-Path $documentFolder 'WinLibs.zip')"
+  Expand-Archive -Path "$(Join-Path $documentFolder 'WinLibs.zip')" -DestinationPath "$(Join-Path $documentFolder WinLibs)" -Force
+  if (Test-Path "C:\mingw64") {
+    Remove-Item "C:\mingw64" -Recurse -Force
+  }
+  Move-Item -Path "$(Join-Path $documentFolder WinLibs)\mingw64" -Destination "C:\mingw64" -Force
+  Remove-Item "$(Join-Path $documentFolder 'WinLibs.zip')"
+  Remove-Item "$(Join-Path $documentFolder WinLibs)" -Recurse -Force
+  Write-Host "WinLibs installed successfully"
+
+  # Downloading Spotify
+  Invoke-WebRequest https://download.scdn.co/SpotifySetup.exe -Outfile "$(Join-Path $downloadFolder 'SpotifySetup.exe')"
+
+  $spotifyProc = Start-Process -FilePath "$(Join-Path $downloadFolder "SpotifySetup.exe")" -ArgumentList "/S" -PassThru
+  $spotifyProc.WaitForExit()
+  Get-Process -Name "Spotify" -ErrorAction SilentlyContinue | Stop-Process -Force
+  Remove-Item "$(Join-Path $downloadFolder "SpotifySetup.exe")" -Force
+  Write-Host "Spotify installed successfully"
 }
